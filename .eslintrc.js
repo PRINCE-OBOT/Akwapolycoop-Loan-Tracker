3d97814081c271c2fff68fa1a6b1ca506038ba64
PRINCE-OBOT <princessamuel45@gmail.com>
2025-08-23 16:47:01 +0100

    Add formatter - prettier and linting

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'plugin:import/recommended',
+    'prettier',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  rules: {
+    // You can override Airbnb rules here if you want
+  },
+};
+
---
+++ src/homepage/template_homepage.js
@@ -0,0 +1,3 @@
+const myName = {
+  home: 'Wema',
+};
