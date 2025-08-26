52247b6c0180cf195ecc4b1eba1176f902bca061
PRINCE-OBOT <princessamuel45@gmail.com>
2025-08-27 00:20:35 +0100

    - Enable scroll to bottom when contact is click - Enable scroll to top when up-arrow is click - Use right icon for email and contact in footer

--- src/homepage/template_homepage.js
+++ src/homepage/template_homepage.js
@@ -1,3 +1,24 @@
 import './template_homepage.css';
 import './assets/reset.css';
 import './assets/font.css';
+
+const buttonContactUs = document.querySelector('.contact-us');
+const backToTop = document.querySelector('.back-to-top');
+
+function scrollToBottom() {
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: 'smooth',
+  });
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
+backToTop.addEventListener('click', scrollToTop);
+
+buttonContactUs.addEventListener('click', scrollToBottom);
