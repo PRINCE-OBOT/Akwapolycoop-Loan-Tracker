15387024308ce4a5fe59781700d8c6f5ef49b1c9
PRINCE-OBOT <princessamuel45@gmail.com>
2025-10-26 17:25:56 +0100

    Fix: display of declineImg even when deposit is approve

--- src/homepage/borrower/borrower-dashboard/myDeposit.js
+++ src/homepage/borrower/borrower-dashboard/myDeposit.js
@@ -3,6 +3,7 @@ import eventBus from '../../module/event-bus/event';
 import indexDB from '../../module/indexDB/indexDB';
 import eyeViewImg from '../../assets/images/eye-view.svg';
 import declineImg from '../../assets/images/delined-loan.svg';
+import approveImg from '../../assets/images/approve-loan.svg';
 
 import registerLocalStorageCustomMethod from '../../module/localStorage/localStorage';
 import { proofOfPayment } from '../../admin/admin-dashboard/proof-of-payment/proof-of-payment';
@@ -29,7 +30,7 @@ const myDeposit = (function createTableHeading() {
        <th>Deposit ID</th>
        <th>Amount</th>
        <th>Status</th>
-       <th>Proof</th>
+       <th>Details</th>
        <th>Date</th>
        <th>Time</th>
      </tr>
@@ -107,7 +108,7 @@ function insertLoanApplicantDataToTr(loanApplicantData) {
        <td>${data.status}</td>
        <td data-view="proofOfPayment">
        <img src="${eyeViewImg}" alt="view proof of payment" class="eye-view"/>
-       View Proof
+       View Detail
        </td>
        <td>${date}</td>
        <td>${time}</td>
@@ -167,7 +168,7 @@ function insertDataToProofOfPayment(result) {
     return;
   }
 
-  proofOfPaymentPreview.src = result.adminProofOfPayment ? result.adminProofOfPayment : declineImg;
+  proofOfPaymentPreview.src = result.status === 'Approve' ? approveImg : declineImg;
 
   const date = getDate(result.actionDate);
   const time = getTime(result.actionDate);
